457161e49d402d3ea58b3898e0656382d7ea44d7
planeset94 <planeset94@gmail.com>
2021-08-31 11:57:51 +0200

    add o2m relation users-dishes

--- app/Dish.php
+++ app/Dish.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Dish extends Model
 {
     protected $table='dishes'; 
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function dishes(){
+        return $this->hasMany(Dish::class);
+
+    }
+
+
 }
