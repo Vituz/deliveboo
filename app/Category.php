1ec1bac0576668a9a4f38ce13dcecced8f2d161f
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-08-31 11:02:12 +0100

    Create Category model and related seeeder and migration. Create Pivot Table category_user and related functions in models.

---
+++ app/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $categories=['Italiano','Indiano','Africano','Cinese','Etnico','Pizzeria','Carne','Pesce','Vegetariano','Vegano','Sushi'];
+
+        foreach ($categories as $cat) {
+            $new_cat= new Category();
+            
+            $new_cat->name=$cat;
+            $new_cat->img=$faker->imageUrl(200,200,null,true);
+
+            $new_cat->save();
+        }
+    }
+}
