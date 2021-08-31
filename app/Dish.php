593d7fe805b9f717b0ea32698fb9aca5fa224198
Vituz <79053442+Vituz@users.noreply.github.com>
2021-08-31 12:51:53 +0200

    add category to RegisterUser

--- app/Dish.php
+++ app/Dish.php
@@ -4,14 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Dish extends Model
 {
-    protected $table='dishes'; 
+    protected $table = 'dishes';
 
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
