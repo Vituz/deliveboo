25d943b64d05b4427a74a53268e109f5d7d9da03
dumi994 <dumi.9944@gmail.com>
2021-09-01 17:43:28 +0200

    fixing routes in layout.admin.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -73,16 +73,16 @@
                         <ul class="nav flex-column py-3">
                             <li class="nav-item nav-pills">
 
-                                <a class="nav-link {{Route::currentRouteName() == ''? 'active': ''}}" href="{{route('admin.dashboard')}}"><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a>
+                                <a class="nav-link {{Route::currentRouteName() == '' ? 'active' : ''}}" href="{{route('admin.dashboard')}}"><i class="fas fa-tachometer-alt mr-2"></i> Dashboard</a>
                             </li>
                             <li class="nav-item nav-pills">
-                                <a class="nav-link {{Route::currentRouteName() == ''? 'active': ''}}" href=""><i class="fas fa-newspaper mr-2"></i> Piatti</a>
+                                <a class="nav-link {{Route::currentRouteName() == '' ? 'active' : ''}}" href="{{route('admin.dishes.index')}}"><i class="fas fa-newspaper mr-2"></i> Piatti</a>
                             </li>
                             <li class="nav-item nav-pills">
-                                <a class="nav-link {{Route::currentRouteName() == ''? 'active': ''}}" href=""><i class="fas fa-newspaper mr-2"></i> Ordini</a>
+                                <a class="nav-link {{Route::currentRouteName() == '' ? 'active': ''}}" href=""><i class="fas fa-newspaper mr-2"></i> Ordini</a>
                             </li>
                             <li class="nav-item nav-pills">
-                                <a class="nav-link {{Route::currentRouteName() == ''? 'active': ''}}" href=""><i class="fas fa-newspaper mr-2"></i> Statistiche</a>
+                                <a class="nav-link {{Route::currentRouteName() == '' ? 'active': ''}}" href=""><i class="fas fa-newspaper mr-2"></i> Statistiche</a>
                             </li>
 
                         </ul>
