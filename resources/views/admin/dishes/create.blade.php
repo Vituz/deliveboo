96586aeef766d09c4f81ae18bb19a52d85224915
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-09-01 10:37:44 +0100

    Establish layouts structure and exdend admin layout to related views. Make home.blade.php the view to redirect to after registering

---
+++ resources/views/admin/dishes/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <h1>Add single dish</h1>
+
+
+@endsection
--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -1,1 +1,9 @@
-<h1>1</h1>
+@extends('layouts.admin')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <h1>Edit single dish</h1>
+
+
+@endsection
