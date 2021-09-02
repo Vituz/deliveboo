ee1a611d0e2e449f0f11e0916b05377986fcb8a0
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-09-02 11:03:44 +0100

    Save dish img in edit form. Roughly style view admin.dishes.show layout.

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -7,8 +7,8 @@
         <div class="show-card col-md-10">
                 <div class="card-top mb-3">
                     <!-- <img src="{{asset('storage/' . $dish->image)}}" alt="{{$dish->name}}"> -->
-                    <img src="{{$dish->img}}" width="500" class="mb-4" alt="{{$dish->name}}">
-                    <h3>King size {{$dish->name}} </h3>
+                    <h3>{{$dish->name}} </h3>
+                    <img src="{{asset('storage/'. $dish->img)}}" alt="{{$dish->name}}" width="400">
                     <p>{{$dish->description}}</p>
                     <p>{{$dish->ingredients}}</p>
                     <span>{{$dish->price}} &euro;</span>
